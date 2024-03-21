96e1429b98600d3c616abda94d53f5cbb162d83b
brian <yatunyi.15075@students.kyu.ac.ke>
2024-03-21 17:36:20 +0300

    3

--- frontend/school_management_system/src/App.jsx
+++ frontend/school_management_system/src/App.jsx
@@ -1,34 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useSelector } from 'react-redux';
+import Homepage from './pages/Homepage';
+import AdminDashboard from './pages/admin/AdminDashboard';
+import StudentDashboard from './pages/student/StudentDashboard';
+import TeacherDashboard from './pages/teacher/TeacherDashboard';
+import LoginPage from './pages/LoginPage';
+import AdminRegisterPage from './pages/admin/AdminRegisterPage';
+import ChooseUser from './pages/ChooseUser';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const { currentRole } = useSelector(state => state.user);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      {currentRole === null &&
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/choose" element={<ChooseUser visitor="normal" />} />
+          <Route path="/chooseasguest" element={<ChooseUser visitor="guest" />} />
+
+          <Route path="/Adminlogin" element={<LoginPage role="Admin" />} />
+          <Route path="/Studentlogin" element={<LoginPage role="Student" />} />
+          <Route path="/Teacherlogin" element={<LoginPage role="Teacher" />} />
+
+          <Route path="/Adminregister" element={<AdminRegisterPage />} />
+
+          <Route path='*' element={<Navigate to="/" />} />
+        </Routes>}
+
+      {currentRole === "Admin" &&
+        <>
+          <AdminDashboard />
+        </>
+      }
+
+      {currentRole === "Student" &&
+        <>
+          <StudentDashboard />
+        </>
+      }
+
+      {currentRole === "Teacher" &&
+        <>
+          <TeacherDashboard />
+        </>
+      }
+    </Router>
   )
 }
 
